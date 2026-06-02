ab9624a8ca714faf3752cce44eb16f5abd41a302
Axeme01 <daniil_balandin01@mail.ru>
2026-06-02 21:14:04 +0700

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,15 @@
+with open('input.txt', 'w', encoding='utf-8') as f:
+    f.write("""Первая строка.
+Вторая строка.
+Третья строка.""")
+lines = 0
+words = 0
+with open('input.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        lines += 1
+        words += len(line.split())
+with open('statistics.txt', 'w', encoding='utf-8') as out:
+    print(f"Строк: {lines}", file=out)
+    print(f"Слов: {words}", file=out)
+print(f"Строк: {lines}")
+print(f"Слов: {words}")
